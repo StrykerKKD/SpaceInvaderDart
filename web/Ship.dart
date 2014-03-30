d00ab9d5e269473f2c875c5cef0aefaecb0d2f0b
StrykerKKD <kkdstryker@gmail.com>
2014-03-31 00:19:05 +0200

    Using Juggler for moving the ship. Remove unused css from html. Adding Ship class which will be used later.

---
+++ web/Ship.dart
@@ -0,0 +1,6 @@
+part of spaceinvader;
+
+class Ship extends Sprite {
+  Ship() {
+  }
+}
--- web/spaceinvaderdart.dart
+++ web/spaceinvaderdart.dart
@@ -1,15 +1,32 @@
+library spaceinvader;
+
+//-----------------------------------------------------------------------------
+
 import 'dart:html' as html;
 import 'package:stagexl/stagexl.dart';
 
+//-----------------------------------------------------------------------------
+
+part 'Ship.dart';
+
+//-----------------------------------------------------------------------------
+
+Stage stage;
+RenderLoop renderLoop;
+ResourceManager resourceManager;
+Juggler juggler;
+Tween tween;
+
 void main() {
   // setup the Stage and RenderLoop
-  var canvas = html.querySelector('#stage');
-  Stage stage = new Stage(canvas);
-  RenderLoop renderLoop = new RenderLoop();
+  html.Element canvas = html.querySelector('#stage');
+  stage = new Stage(canvas);
+  renderLoop = new RenderLoop();
+  juggler = renderLoop.juggler;
   renderLoop.addStage(stage);
   
   //Making a resourceManager and 
-  ResourceManager resourceManager = new ResourceManager()
+  resourceManager = new ResourceManager()
     ..addBitmapData("ship", "./assets/playerShip1_blue.png");
   
   //loading resources via resourceManager
@@ -19,6 +36,8 @@ void main() {
     
     //add the ship to the stage
     stage.addChild(ship);
+    ship.x = 400;
+    ship.y = 300;
     
     //the stage needs to be an interactive object,
     //so we can use it for keyboard events
@@ -30,24 +49,32 @@ void main() {
     const right_arrow = 39;
     const down_arrow = 40;
 
+    //speed of the ship
+    int speed = 20;
 
     //Event handling for cursor keys, onKeyDown return a Keyboard event 
     stage.onKeyDown.listen((value){
       //print(value.keyCode);
-      
+
+      tween = new Tween(ship, 0.1, TransitionFunction.linear);
+
       //switch statemant to decide which cursor keys was down
       switch(value.keyCode){
           case left_arrow:
-            ship.x += -10;
+            tween.animate.x.to(ship.x-speed);
+            juggler.add(tween);
             break;
           case up_arrow:
-            ship.y += -10;
+            tween.animate.y.to(ship.y-speed);
+            juggler.add(tween);
             break;
           case right_arrow:
-            ship.x += 10;
+            tween.animate.x.to(ship.x+speed);
+            juggler.add(tween);
             break;
           case down_arrow:
-            ship.y += 10;
+            tween.animate.y.to(ship.y+speed);
+            juggler.add(tween);
             break;
       }
         
