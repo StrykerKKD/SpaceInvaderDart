11feb396d2721b227eaed4ff0131746cf83f24e2
StrykerKKD <kkdstryker@gmail.com>
2014-03-31 19:31:25 +0200

    Implementing the ship class

--- web/Ship.dart
+++ web/Ship.dart
@@ -1,6 +1,32 @@
 part of spaceinvader;
 
-class Ship extends Sprite {
-  Ship() {
+class Ship extends Bitmap implements Animatable {
+
+  num _vx, _vy;
+  bool movingLeft = false;
+  bool movingRight = false;
+  bool movingUp = false;
+  bool movingDown = false;
+
+  Ship(BitmapData bitmapData,this._vx,this._vy) : super(bitmapData) {
+    this.pivotX = bitmapData.width / 2;
+    this.pivotY = bitmapData.height / 2;
+    this.x = 400;
+    this.y = 300;
   }
+
+  bool advanceTime(num time){
+    if(movingLeft){
+      x = x - _vx * time;
+    }else if(movingRight){
+      x = x + _vx * time;
+    }
+    if(movingUp){
+      y = y - _vy * time;
+    }else if(movingDown){
+      y = y + _vy * time;
+    }
+    return true;
+  }
+
 }
